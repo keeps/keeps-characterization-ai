4969af7e852306b5fc48f215216683fb5d5bedf5
Sébastien Leroux <sleroux@mandel.(none)>
2014-01-09 11:18:35 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
 keeps-characterization-ai
-=========================
+==========================
 
-Characterization tool for Adobe Illustrator files, made by KEEP SOLUTIONS.
+Characterization tool for ai files, made by KEEP SOLUTIONS.
+
+
+## Build & Use
+
+To build the application simply clone the project and execute the following Maven command: `mvn clean package`  
+The binary will appear at `target/ai-characterization-tool-1.0-SNAPSHOT-jar-with-dependencies.jar`
+
+To see the usage options execute the command:
+
+```bash
+$ java -jar target/ai-characterization-tool-1.0-SNAPSHOT-jar-with-dependencies.jar -h
+usage: java -jar [jarFile]
+ -f <arg> file to analyze
+ -h       print this message
+ -v       print this tool version
+```
+
+## Tool Output Example
+```bash
+<aiCharacterizationResult>
+    <features>
+        <item>
+            <key>Artifacts.verbose</key>
+            <value>true</value>
+        </item>
+        <item>
+            <key>Background_color</key>
+            <value>white</value>
+        </item>
+        <item>
+            <key>Base_type</key>
+            <value>ColorSeparation</value>
+        </item>
+        <item>
+            <key>Border_color</key>
+            <value>cmyk(223,223,223,0)</value>
+        </item>
+        ( .... )
+    </features>
+    <valid>true</valid>
+</aiCharacterizationResult>
+
+
+```
+
+## License
+
+This software is available under the [LGPL version 3 license](LICENSE).
+
---
+++ src/main/java/pt/keep/validator/AiCharacterizationTool.java
@@ -0,0 +1,186 @@
+package pt.keep.validator;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+
+import pt.keep.validator.results.Result;
+import pt.keep.validator.utils.CommandLine;
+
+public class AiCharacterizationTool 
+{
+	private String version = "1.0";
+	
+	
+	public String getVersion(){
+		return version;
+	}
+	public boolean isUniconvertoInstalled(){
+		boolean installed=false;
+		try{
+			String execOut = CommandLine.exec(Arrays.asList("uniconvertor"),null);
+			return true;
+		}catch(IOException e){
+		}
+		return installed;
+	}
+	
+	public boolean isImageMagickInstalled(){
+		boolean installed=false;
+		try{
+			String execOut = CommandLine.exec(Arrays.asList("identify"),null);
+			return true;
+		}catch(IOException e){
+		}
+		return installed;
+	}
+	
+	
+	public String run(File f) throws IOException, JAXBException{
+		if(isUniconvertoInstalled() && isImageMagickInstalled()){
+			List<String> command = new ArrayList<String>(Arrays.asList("identify", "-verbose",f.getPath()));
+			String identifyOutput = CommandLine.exec(command, null);
+			Result res = new Result();
+			res.setValid(false);
+			res.setFeatures(new Hashtable<String, String>());
+			try{
+				
+				Map<String,String> properties = extractMetadata(identifyOutput);
+				
+				if(properties.containsKey("Format") && properties.get("Format").equalsIgnoreCase("PDF (Portable Document Format)")){
+					res.setValid(true);
+				}
+				
+				res.setFeatures(properties);
+			}catch(Exception e){
+				
+			}
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			JAXBContext jaxbContext = JAXBContext.newInstance(Result.class);
+			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			jaxbMarshaller.marshal(res, bos);
+			return bos.toString("UTF-8");
+		}else{
+			return null;
+		}
+	}
+	
+	private Map<String, String> extractMetadata(String identifyOutput) {
+		
+		Map<String,String> metadata = new TreeMap<String, String>();
+		try{
+			String last2="",last4="",last6="";
+			
+			for(String s : identifyOutput.split("\n")){
+				if(s.contains(":")){
+					String key = s.substring(0,s.indexOf(":")).trim().replace(" ", "_");
+					String value = s.substring(s.indexOf(":")+1).trim();
+					if(s.contains(": ")){
+						key = s.substring(0,s.indexOf(": ")).trim().replace(" ", "_");
+						value = s.substring(s.indexOf(": ")+1).trim();
+					}
+					if(key.contains(":")){
+						String[] tokens = key.split(":");
+						String newKey="";
+						
+						for(int i=0;i<tokens.length;i++){
+							newKey+=(i==0)?tokens[i]:tokens[i].substring(0, 1).toUpperCase() + tokens[i].substring(1); ;
+						}
+						key=newKey;
+					}
+					if(s.startsWith("      ")){
+						last6=key;
+						key=last2+"."+last4+"."+key;
+						if(!value.equals("")){
+							metadata.put(key, value);
+						}
+					}else if(s.startsWith("    ")){
+						last4=key;
+						key=last2+"."+key;
+						if(!value.equals("")){
+							metadata.put(key, value);
+						}
+					}else if(s.startsWith("  ")){
+						last2=key;
+						if(!value.equals("")){
+							metadata.put(key, value);
+						}
+					}
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return metadata;
+	}
+
+	private void printHelp(Options opts) {
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("java -jar [jar file]", opts);
+	}
+	
+	private void printVersion() {
+		System.out.println(version);
+	}
+	
+    public static void main( String[] args )
+    {
+
+    	try {
+    		AiCharacterizationTool act = new AiCharacterizationTool();
+			Options options = new Options();
+			options.addOption("f", true, "file to analyze");
+			options.addOption("v", false, "print this tool version");
+			options.addOption("h", false, "print this message");
+
+			CommandLineParser parser = new GnuParser();
+			org.apache.commons.cli.CommandLine cmd = parser.parse(options, args);
+
+			if (cmd.hasOption("h")) {
+				act.printHelp(options);
+				System.exit(0);
+			}
+			
+			if (cmd.hasOption("v")) {
+				act.printVersion();
+				System.exit(0);
+			}
+
+			if (!cmd.hasOption("f")) {
+				act.printHelp(options);
+				System.exit(0);
+			}
+
+			File f = new File(cmd.getOptionValue("f"));
+			if (!f.exists()) {
+				System.out.println("File doesn't exist");
+				System.exit(0);
+			}
+			
+			String res = act.run(f);
+			if(res!=null){
+				System.out.println(res);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+    }
+}
